544f210f6f2b70adcd157de9456f9c934006a9e5
cverges1 <122914738+cverges1@users.noreply.github.com>
2023-07-06 11:06:20 -0700

    preparing to deploy to heroku

--- server.js
+++ server.js
@@ -37,5 +37,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
 });
